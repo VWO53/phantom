4181cd7e7a4af58a61ce79f911b8509c2c78df7c
luttappi <64356181+luttappi@users.noreply.github.com>
2020-06-17 22:01:33 +0530

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -31,7 +31,7 @@ PM_START_TEXT = """
 
 I am a Powerful *Group Management Bot* With Some Special Features.
 
-Maintained By This [Legend](userid
+Maintained By This [Legend](tg://user?id={})
 
 You can find my list of available commands with /help.
  
